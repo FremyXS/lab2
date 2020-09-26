20adda2a710f8198445f9e6e4cb0469e90b2bf77
FremyXS <70861067+FremyXS@users.noreply.github.com>
2020-09-26 10:23:40 +0500

    3

--- lab02.cs
+++ lab02.cs
@@ -8,51 +8,47 @@ namespace lab02_ver2
         static void Main(string[] args)
         {
             Console.WriteLine("Выберите шахматную фигуру:");
-            int shf;
+            int ShahFig;
             Console.WriteLine("Король - 1, Ферзь - 2, Ладья - 3, Слон - 4, Конь - 5, Пешка - 6 ");
             do
             {
-                shf = Convert.ToInt32(Console.ReadLine());
-                if (shf < 1 || shf > 6)
+                ShahFig = Convert.ToInt32(Console.ReadLine());
+                if (ShahFig < 1 || ShahFig > 6)
                     Console.WriteLine("Нет такой фигуры. Попробуйте ещё раз");
                 else
                     break;
-            } while (shf < 1 || shf > 6);
+            } while (ShahFig < 1 || ShahFig > 6);
+            Console.WriteLine("Введите начальные координаты:\nПо горизонтали - ABCDEFGH\nПо вертикали - 12345678\nПример C5");
+            int StartVert = 0;
+            int StartHoriz = 0;
+            ChekKoord(ref StartHoriz, ref StartVert);
             Console.WriteLine("Введите конечные координаты:\nПо горизонтали - ABCDEFGH\nПо вертикали - 12345678\nПример C5");
-            int endver = 0;
-            int endgor = 0;
-            ProvFig(ref endgor, ref endver);
-            Console.WriteLine("Введите конечные координаты:\nПо горизонтали - ABCDEFGH\nПо вертикали - 12345678\nПример C5");
-            int endgor2 = 0;
-            int endver2 = 0;
-            ProvFig(ref endgor2, ref endver2);
-            if (shf == 1)
-                Korol(endgor, endgor2, endver, endver2);
-            else if (shf == 2)
-                Ferz(endgor, endgor2, endver, endver2);
-            else if (shf == 3)
-                Lad(endgor, endgor2, endver, endver2);
-            else if (shf == 4)
-                Slon(endgor, endgor2, endver, endver2);
-            else if (shf == 5)
-                Kon(endgor, endgor2, endver, endver2);
-            else if (shf == 6)
-                Peshka(endgor, endgor2, endver, endver2);
+            int EndHoriz = 0;
+            int EndVert = 0;
+            ChekKoord(ref EndHoriz, ref EndVert);
+            if (ShahFig == 1)
+                Korol(StartHoriz, EndHoriz, StartVert, EndVert);
+            else if (ShahFig == 2)
+                Ferz(StartHoriz, EndHoriz, StartVert, EndVert);
+            else if (ShahFig == 3)
+                Lad(StartHoriz, EndHoriz, StartVert, EndVert);
+            else if (ShahFig == 4)
+                Slon(StartHoriz, EndHoriz, StartVert, EndVert);
+            else if (ShahFig == 5)
+                Kon(StartHoriz, EndHoriz, StartVert, EndVert);
+            else if (ShahFig == 6)
+                Peshka(StartHoriz, EndHoriz, StartVert, EndVert);
         }
-        static void ProvFig(ref int x, ref int y)
+        static void ChekKoord(ref int x, ref int y) //x - StartHoriz, y - StartVert
         {
-            string koor1;
-            string gor;
-            char gk;
+            string Koord;
             int goriz;
             int vert;
             do
             {
-                koor1 = Console.ReadLine();
-                gk = koor1[0];
-                goriz = Convert.ToInt16(gk);
-                gor = Convert.ToString(koor1[1]);
-                vert = Convert.ToInt32(gor);
+                Koord = Console.ReadLine();
+                goriz = Convert.ToInt16(Koord[0]);
+                vert = Convert.ToInt32(Convert.ToString(Koord[1]));
                 if (goriz < 65 || goriz > 72 || vert < 1 || vert > 8)
                     Console.WriteLine("Не верно. Попробуй ещё раз.");
                 else
@@ -63,7 +59,7 @@ namespace lab02_ver2
                 }
             } while (goriz < 65 || goriz > 72 || vert < 1 || vert > 8);
         }
-        static void Korol(int x1, int x2, int y1, int y2)
+        static void Korol(int x1, int x2, int y1, int y2) //x1 - StartHoriz, x2 -EndHoriz, y1 - StartVert, y2 - EndVert
         {
             int razngor = Math.Abs(x1 - x2);
             int raznver = Math.Abs(y1 - y2);
@@ -76,7 +72,7 @@ namespace lab02_ver2
             else
                 Console.WriteLine("Не верно");
         }
-        static void Kon(int x1, int x2, int y1, int y2)
+        static void Kon(int x1, int x2, int y1, int y2) //x1 - StartHoriz, x2 -EndHoriz, y1 - StartVert, y2 - EndVert
         {
             int razngor = Math.Abs(x1 - x2);
             int raznver = Math.Abs(y1 - y2);
@@ -87,7 +83,7 @@ namespace lab02_ver2
             else
                 Console.WriteLine("Не верно");
         }
-        static void Ferz(int x1, int x2, int y1, int y2)
+        static void Ferz(int x1, int x2, int y1, int y2) //x1 - StartHoriz, x2 -EndHoriz, y1 - StartVert, y2 - EndVert
         {
             int razngor = Math.Abs(x1 - x2);
             int raznver = Math.Abs(y1 - y2);
@@ -100,7 +96,7 @@ namespace lab02_ver2
             else
                 Console.WriteLine("Не верно");
         }
-        static void Lad(int x1, int x2, int y1, int y2)
+        static void Lad(int x1, int x2, int y1, int y2) //x1 - StartHoriz, x2 -EndHoriz, y1 - StartVert, y2 - EndVert
         {
             int razngor = Math.Abs(x1 - x2);
             int raznver = Math.Abs(y1 - y2);
@@ -109,7 +105,7 @@ namespace lab02_ver2
             else
                 Console.WriteLine("Не верно");
         }
-        static void Slon(int x1, int x2, int y1, int y2)
+        static void Slon(int x1, int x2, int y1, int y2) //x1 - StartHoriz, x2 -EndHoriz, y1 - StartVert, y2 - EndVert
         {
             int razngor = Math.Abs(x1 - x2);
             int raznver = Math.Abs(y1 - y2);
@@ -120,7 +116,7 @@ namespace lab02_ver2
             else
                 Console.WriteLine("Не верно");
         }
-        static void Peshka(int x1, int x2, int y1, int y2)
+        static void Peshka(int x1, int x2, int y1, int y2) //x1 - StartHoriz, x2 -EndHoriz, y1 - StartVert, y2 - EndVert
         {
             int razngor = Math.Abs(x1 - x2);
             int raznver = Math.Abs(y1 - y2);
